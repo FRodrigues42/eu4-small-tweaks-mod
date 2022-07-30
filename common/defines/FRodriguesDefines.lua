09851f13e3490233e898ed3aa6d515ea129759a7
Fernando Rodrigues <FRodrigues42@users.noreply.github.com>
2022-07-30 02:44:54 +0100

    removal of ages and building tweaks

--- common/defines/FRodriguesDefines.lua
+++ common/defines/FRodriguesDefines.lua
@@ -8,12 +8,15 @@ NDefines.NEconomy.DEBASE_MAX_CORRUPTION = 20
 NDefines.NEconomy.COT_DOWNGRADE_PRESTIGE_COST = 0
 
 NDefines.NMilitary.WARSCORE_MAX_FROM_BATTLES = 80
-NDefines.NMilitary.WARGOAL_MAX_BONUS = 35
-NDefines.NMilitary.DEFAULT_WARGOAL_TICKINGWARSCORE_BONUS = 0.7
+NDefines.NMilitary.WARGOAL_MAX_BONUS = 50
+NDefines.NMilitary.DEFAULT_WARGOAL_TICKINGWARSCORE_BONUS = 1
 NDefines.NMilitary.DEFAULT_WARGOAL_BATTLESCORE_BONUS = 10
-NDefines.NMilitary.SUPERIORITY_WARGOAL_WARSCORE_THRESHOLD = 33
+NDefines.NMilitary.SUPERIORITY_WARGOAL_WARSCORE_THRESHOLD = 30
 
-NDefines.NReligion.MAX_CHURCH_POWER = 300
+-- BACK_LINE_MORALE_DAMAGE_TAKEN_MODIFIER Does not work in the 1.33.3 patch
+NDefines.NMilitary.BACK_LINE_MORALE_DAMAGE_TAKEN_MODIFIER = 0.7	-- Multiplier for morale damage taken by backrow
+NDefines.NMilitary.BACK_LINE_STRENGTH_DAMAGE_TAKEN_MODIFIER = 0.1	-- Multiplier for strength damage taken by backrow
+NDefines.NMilitary.OVERRUN_FACTOR = 7							-- How much stronger one side of a battle must be to cause the other side to be overrun (integer)
 
 NDefines.NAI.DIPLOMATIC_ACTION_KNOWLEDGE_SHARING_ACCEPTANCE_MULT = 1
 
